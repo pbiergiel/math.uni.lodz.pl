6034d4ddaf85695bebf8da33341cf2d4853c2f44
pbiergiel <paatrychaa@gmail.com>
2016-01-13 18:35:56 +0100

    lesson 2 homework

--- lesson-2/src/test/java/com/gft/structuraltest/testconference/registrationdata/AnswerEntityTest.java
+++ lesson-2/src/test/java/com/gft/structuraltest/testconference/registrationdata/AnswerEntityTest.java
@@ -12,4 +12,77 @@ public class AnswerEntityTest{
         assertFalse(an1.equals(an2));
     }
     
+    @Test
+    public void shouldReturnTrueWhenObjectEqualsObject(){
+        AnswerEntity an1 = new AnswerEntity();
+        assertTrue(an1.equals(an1));
+    }
+    
+    @Test
+    public void shouldReturnFalseWhenObjectClassNotEqualsAnotherObjectClass(){
+        AnswerEntity an1 = new AnswerEntity();
+        Object an2 = new Object();
+        assertFalse(an1.equals(an2));
+    }
+    
+    @Test
+    public void shouldReturnFalseWhenFirstObjectTextIsNullAndTheOtherIsNotNull(){
+        AnswerEntity an1 = new AnswerEntity();
+        AnswerEntity an2 = new AnswerEntity();
+        an2.setAnswerText("Answer");
+        assertFalse(an1.equals(an2));
+    }
+    
+    @Test
+    public void shouldReturnFalseWhenFirstObjectTextAndSecondObjectTextAreNotEqual(){
+        AnswerEntity an1 = new AnswerEntity();
+        AnswerEntity an2 = new AnswerEntity();
+        an1.setAnswerText("Answer1");
+        an2.setAnswerText("Answer2");
+        assertFalse(an1.equals(an2));
+    }
+    
+    @Test
+    public void shouldReturnFalseWhenFirstObjectIdIsNullAndSecondObjectIdIsNotNull(){
+        AnswerEntity an1 = new AnswerEntity();
+        AnswerEntity an2 = new AnswerEntity();
+        an2.setId(1L);
+        assertFalse(an1.equals(an2));
+    }
+    
+    @Test
+    public void shouldReturnFalseWhenFirstObjectIdAndSecondObjectIdAreNotEqual(){
+        AnswerEntity an1 = new AnswerEntity();
+        AnswerEntity an2 = new AnswerEntity();
+        an1.setId(1L);
+        an2.setId(2L);
+        assertFalse(an1.equals(an2));
+    }
+    
+    @Test
+    public void shouldReturnFalseWhenFirstObjectQuestionIsNullAndSecondObjectQuestionIsNotNull(){
+        AnswerEntity an1 = new AnswerEntity();
+        AnswerEntity an2 = new AnswerEntity();
+        an2.setQuestion(new QuestionEntity());
+        assertFalse(an1.equals(an2));
+    }
+    
+    @Test
+    public void shouldReturnFalseWhenFirstObjectQuestionAndSecondQuestionAreNotEqual(){
+        AnswerEntity an1 = new AnswerEntity();
+        AnswerEntity an2 = new AnswerEntity();
+        QuestionEntity question = new QuestionEntity();
+        question.setId(1L);
+        an1.setQuestion(question);
+        an2.setQuestion(new QuestionEntity());
+        assertFalse(an1.equals(an2));
+    }
+    
+    @Test
+    public void shouldReturnTrueWhenTwoObjectsAreEqual(){
+        AnswerEntity an1 = new AnswerEntity();
+        AnswerEntity an2 = new AnswerEntity();
+        assertTrue(an1.equals(an2));
+    }
+    
 }
